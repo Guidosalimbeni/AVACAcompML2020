1e78b557516dd06bed5e37865647a3f9dba8c487
Guidosalimbeni <guido.salimbeni@gmail.com>
2020-06-12 19:30:16 +0100

    ai turn and not ai turn

--- Assets/_scripts/AI new/AI_Calculator_score.cs
+++ Assets/_scripts/AI new/AI_Calculator_score.cs
@@ -10,6 +10,8 @@ public class AI_Calculator_score : MonoBehaviour
     private BarracudaNNfromDatabase barracudaNNfromDatabase;
     private BarracudaOpenCvFeature barracudaOpenCvFeature;
     private BarracudaFinalOut barracudaFinalOut;
+    private Comp_agent_float_move_child[] agents;
+    private int maxFramesForRound;
 
     public int steps = 6;
     public float target = 0.95f;
@@ -17,8 +19,9 @@ public class AI_Calculator_score : MonoBehaviour
     public bool movetotarget = true;
     public bool inferenceMode = false;
     public bool AIturn = false;
-    
-   
+
+    public event Action<int> OnFramesCountChanged;
+
     public bool buttonRunRobot { get; set; }
 
     private void Awake()
@@ -29,6 +32,12 @@ public class AI_Calculator_score : MonoBehaviour
         barracudaNNfromDatabase = FindObjectOfType<BarracudaNNfromDatabase>();
         barracudaOpenCvFeature = FindObjectOfType<BarracudaOpenCvFeature>();
         barracudaFinalOut = FindObjectOfType<BarracudaFinalOut>();
+        agents = FindObjectsOfType<Comp_agent_float_move_child>();
+        
+        for (int i = 0; i < agents.Length; i++)
+        {
+            maxFramesForRound = agents[i].MaxStep; // they are all the same so taking last one
+        }
     }
 
     private void FixedUpdate()
@@ -53,6 +62,11 @@ public class AI_Calculator_score : MonoBehaviour
         {
             if (AIturn == true)
             {
+                for (int i = 0; i < agents.Length; i++)
+                {
+                    agents[i].enabled = true;
+                }
+
                 frames++;
 
                 if (frames % steps == 0)
@@ -68,12 +82,35 @@ public class AI_Calculator_score : MonoBehaviour
 
             if(AIturn == false)
             {
+                frames++;
                 // scoring it is trigger by event on finger up
+                
+                for (int i = 0; i < agents.Length; i++)
+                {
+                    agents[i].enabled = false;
+                }
+
+
             }
             
 
 
         }
+
+        if (OnFramesCountChanged != null)
+        {
+            OnFramesCountChanged(frames);
+        }
+
+        if (frames > maxFramesForRound)
+        {
+            // start couroutine to display score and ask to continue if round AI completed
+            AIturn = !AIturn;
+            frames = 0;
+
+        }
+
+        
 
     }
     
